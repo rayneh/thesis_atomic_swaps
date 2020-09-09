2f1358b0cef78a4f55f21223486af129e45fb96a
rayneh <rayne@druckwelle-hq.de>
2020-09-09 17:32:23 +0200

    started tests chapter

--- aswaps/src/main/java/swaps/App.java
+++ aswaps/src/main/java/swaps/App.java
@@ -12,31 +12,49 @@ public class App
     {
         boolean run = true;
 
+        System.out.println("----------------------------------");
+        System.out.println("Welcome to atomic cross-swaps prototype");
+        System.out.println("IMPORTANT NOTICE: all three chains need to be running and mining!");
+        System.out.println("               ");
+
         while(run) {
-            System.out.println("Welcome to atomic cross-swaps prototype");
             System.out.println("               ");
             System.out.println("please select one menu entry:");
             System.out.println("               ");
             System.out.println("0 - run swap protocol");
             System.out.println("1 - run transactions per second (tps)");
-            System.out.println("2 - run swaps per hour (sph)");
-            System.out.println("3 - run wrong secret test");
-            System.out.println("4 - exit");
-
+            System.out.println("2 - run swaps per hour (sph) - (print pre-measured testing results!)");
+            System.out.println("3 - exit");
+            System.out.println("----------------------------------");
 
             Scanner scanner = new Scanner(System.in);
             int menuEntry = scanner.nextInt();
 
             switch (menuEntry) {
-                case 0: SwapProtocol swapProtocol = new SwapProtocol();
+                case 0: long startTime = System.nanoTime();
+                        SwapProtocol swapProtocol = new SwapProtocol();
+                        long endTime = System.nanoTime() - startTime;
+                        System.out.println("time in nanos: " + endTime);
                         break;
-                case 1:     // TODO: run tps here
+                case 1: Evaluation evaluation = new Evaluation();
+                        evaluation.runTPS();
                         break;
-                case 2:     // TODO: run sph here
+                case 2: long run0 = 377392865700L;
+                        long run1 = 435784403900L;
+                        long run2 = 423182624900L;
+                        long average = (run0 + run1 + run2) / 3;
+                        long oneHour = 3600000000000L;
+
+                        System.out.println("----------------------------------");
+                        System.out.println("Test run #1 result in ns: " + run0);
+                        System.out.println("Test run #2 result in ns: " + run1);
+                        System.out.println("Test run #3 result in ns: " + run2);
+                        System.out.println("Average time in ns: " + average);
+                        System.out.println("  ");
+                        System.out.println("Swaps per Hour (SPH): " + (oneHour / average));
+                        System.out.println("----------------------------------");
                         break;
-                case 3:     // TODO: run wrong secret here
-                        break;
-                case 4: run = false;
+                case 3: run = false;
                         break;
                 default: System.out.println("entry not available, please choose a menu entry");
                          break;
--- aswaps/src/main/java/swaps/Evaluation.java
+++ aswaps/src/main/java/swaps/Evaluation.java
@@ -1,8 +1,36 @@
 package swaps;
 
+import java.io.IOException;
+
 public class Evaluation {
+    private Pools pools;
+    private Wallet tps;
+    private String tpsAddress = "0x0454ad757f4769e1648ba6ca6a8504134e32e9fe";
 
     Evaluation () {
+        this.pools = new Pools();
+        this.init();
+    }
+
+    void init() {
+        this.tps = new Wallet("src\\wallets\\TPS-TestWallet-UTC--2020-09-09T14-34-51.286833900Z--0454ad757f4769e1648ba6ca6a8504134e32e9fe", "1234", "alice");
+    }
+
+    void runTPS() {
+        long startTime = System.nanoTime();
+
+        try {
+            for( int i = 0; i < 10 ; i++ ) {
+                this.pools.sendBitCoin(tpsAddress);
+            }
+
+            System.out.println("new balance: " + this.tps.getBalance(this.tpsAddress));
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        long endTime = System.nanoTime() - startTime;
+        System.out.println("time to send 10 transactions in nanos: " + endTime);
 
     }
 }
